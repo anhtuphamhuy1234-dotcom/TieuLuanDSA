8a6e68200637d23a056a1952d981df5339c9a318
phamhuyanhtu <anhtuphamhuy1234@gmail.com>
2026-08-29 15:47:48 +0700

    update code

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,2 +1,67 @@
 #include <stdio.h>
+typedef struct {
+	char maSach[10] ;
+	char tenSach[50] ;
+	char tacGia[50] ;
+	int namXuatBan ;
+	int soLuong ;
+}Sach;
+typedef struct node {
+	Sach data ;
+	struct node *next ;
+}Node;
+Node *head = NULL ;
+Node* taoNode (Sach s){
+	Node *p = (Node*)malloc(sizeof(Node)) ;
+	if(p == NULL){
+		printf("Khong du bo nho\n") ;
+		return NULL ;
+	}
+	p->data = s ;
+	p->next = NULL ;
+	return p ;
+}
+void themCuoi(Node **head , Sach s){
+	Node *p = taoNode(s) ;
+	if(p == NULL) return ;
+	if(*head == NULL){
+		*head = p ;
+	} else {
+		Node *temp = *head ;
+		while (temp->next != NULL){
+			temp = temp->next ;
+		}
+		temp->next = p ;
+	}
+}
+void docFile(Node **head , char *filename){
+	FILE *f = fopen(filename , "r") ;
+	if(f == NULL){
+		printf("Khong mo duoc file\n") ;
+		return ;
+	}
+	Sach s ;
+	 while (fscanf(f, "%19[^|]|%99[^|]|%49[^|]|%d|%d\n",
+                  s.maSach,
+                  s.tenSach,
+                  s.tacGia,
+                  &s.namXuatBan,
+                  &s.soLuong) == 5) {
+
+        themCuoi(head, s);
+    }
+    fclose(f) ;
+}
+void xuatDanhSach(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        printf("Ma sach: %s\n", temp->data.maSach);
+        printf("Ten sach: %s\n", temp->data.tenSach);
+        printf("Tac gia: %s\n", temp->data.tacGia);
+        printf("Nam XB: %d\n", temp->data.namXuatBan);
+        printf("So luong: %d\n", temp->data.soLuong);
+        printf("----------------------\n");
+        temp = temp->next;
+    }
+}
 
